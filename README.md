48cdbe0b71af35637406921d5456de8169a721e0
moktadir-mirul <moktadir.mirul@gmail.com>
2025-08-22 18:13:43 +0600

    readme file done

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 **Created by Mirul Moktadir Khan**
 
-- This is a server side for swift shop. 
+- This is the server side for *[Swift Shop](https://swift-shop-eight.vercel.app/)*. Products can be added and viewed. Express js and mongoDB is used for database and server connection.
+
+## Live Site - https://swift-shop-server-ruddy.vercel.app
 
  ## Technology Used:
  - ExpressJS
--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(express.json());
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+
+    // await client.connect();
 
     const swiftCollection = client.db("shopDB").collection("swiftCollection");
 
@@ -44,9 +44,9 @@ async function run() {
         const result = await swiftCollection.findOne(filter);
         res.send(result);
     })
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
   }
