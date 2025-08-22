4d502025c3153349718fb5abbc5fa9c87a08b8f7
moktadir-mirul <moktadir.mirul@gmail.com>
2025-08-22 16:29:22 +0600

    readme file added

---
+++ README.md
@@ -0,0 +1,11 @@
+# Swift Shop Server
+
+**Created by Mirul Moktadir Khan**
+
+- This is a server side for swift shop. 
+
+ ## Technology Used:
+ - ExpressJS
+ -MongoDB
+ -CORS
+ -dotenv
--- index.js
+++ index.js
@@ -4,9 +4,54 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mnwmrsu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const swiftCollection = client.db("shopDB").collection("swiftCollection");
+
+    app.post("/products", async(req, res) => {
+        const doc = req.body;
+        const result = await swiftCollection.insertOne(doc);
+        res.send(result);
+    })
+
+    app.get("/products", async (req, res) => {
+        const result = await swiftCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get("/products/:id", async(req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const result = await swiftCollection.findOne(filter);
+        res.send(result);
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Swift Shop Database is Here!');
